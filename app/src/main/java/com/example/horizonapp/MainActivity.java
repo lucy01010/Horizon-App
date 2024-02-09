e67944720b6fc64360c492467256a41c5af37550
lucy01010 <lusine.hovsepyan.y@gmail.com>
2024-02-09 20:11:57 +0400

    still navigation bar error (+ category)

--- app/src/main/java/com/example/horizonapp/MainActivity.java
+++ app/src/main/java/com/example/horizonapp/MainActivity.java
@@ -1,36 +1,21 @@
 package com.example.horizonapp;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.example.horizonapp.ui.home.HomeFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.horizonapp.databinding.ActivityMainBinding;
+import com.example.horizonapp.ui.home.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
-
 
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
@@ -40,72 +25,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView=findViewById(R.id.bottomNavView);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
 
+           bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int itemId = item.getItemId();
 
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
-            @Override
-            public void onNavigationItemReselected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                loadFragment(new HomeFragment(),false);
+                    if (itemId == R.id.navHome) {
+                        loadFragment(new HomeFragment(), true);
+                    } else if (itemId == R.id.navMap) {
+                        loadFragment(new MapFragment(), true);
+                    } else if (itemId == R.id.navSearch) {
+                        loadFragment(new SearchFragment(), true);
+                    } else if (itemId == R.id.navProfile) {
+                        loadFragment(new ProfileFragment(), true);
+                    }
 
-                if (itemId == R.id.navHome) {
-                    loadFragment(new SearchFragment(),true);
-                } else if (itemId == R.id.navMap) {
-                    loadFragment(new MapFragment(),true);
-                } else if (itemId == R.id.navSearch) {
-                    loadFragment(new SearchFragment(),true);
-                } else {
-                    // nav Profile
-                   loadFragment(new ProfileFragment(),true);
+                    return true;
                 }
-            }
-        });
+            });
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_daily_suggestions, R.id.nav_favorite)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-         getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
-    private void loadFragment(Fragment fragment,boolean isAppInitialized) {
-
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        if(isAppInitialized){
-            fragmentTransaction.add(R.id.frameLayout,fragment);
-        }else{
-            fragmentTransaction.add(R.id.frameLayout,fragment);
-
+        if (isAppInitialized) {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
         }
 
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/horizonapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/horizonapp/ui/home/HomeFragment.java
@@ -8,12 +8,22 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.horizonapp.R;
+import com.example.horizonapp.adapters.HomeHorAdapter;
+import com.example.horizonapp.models.HomeHorModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView homeHorizontalRec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
 
@@ -21,6 +31,19 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home,container,false);
 
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+
+        homeHorModelList = new ArrayList<>();
+        homeHorModelList.add(new HomeHorModel(R.drawable.museum,"Museum"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.fortress,"Fortress"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.church,"Church"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.monastery,"Monastery"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(),homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
         return root;
 
 
